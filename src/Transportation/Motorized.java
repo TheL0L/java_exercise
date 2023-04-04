3e83d3ff81d9895a0f43714f50f799fce5533bb3
yaruboy011@gmail.com <yaruboy011@gmail.com>
2023-04-04 17:59:45 +0300

    added Motorized and Notmotorized

--- src/Transportation/Motorized.java
+++ src/Transportation/Motorized.java
@@ -2,5 +2,8 @@ package Transportation;
 
 public interface Motorized
 {
-
+	public float GetAvgFuel();
+	public float GetMotorLife();
+	public void SetAvgFuel(float value);
+	public void SetMotorLife(float value);
 }
