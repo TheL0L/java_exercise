eab0035fcfcec0ff31ced48480710ad29662b5ee
Sergei <sergeiyakima1@gmail.com>
2023-04-08 21:07:17 +0300

    added documentation for commercial interface

--- src/Transportation/iCommercial.java
+++ src/Transportation/iCommercial.java
@@ -1,8 +1,19 @@
 package Transportation;
 
+/*
+ * Interface for vehicles being used in a commercial manner.
+ */
 public interface iCommercial
 {
+	/*
+	 * Types of commercial licenses available.
+	 */
 	public static enum CommercialUse { MINI, LIMIT, UNLIMIT };
 	
+	/*
+	 * Method for retrieving the vehicle's commercial license.
+	 * 
+	 * @return string containing the name of the used license.
+	 */
 	public String GetCommercialUsage();
 }
