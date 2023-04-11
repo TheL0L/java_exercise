6d5617fbd93e252ebe1c598c92cf009568b75700
Sergei <sergeiyakima1@gmail.com>
2023-04-11 09:31:19 +0300

    added documentation for landvehicle class

--- src/Transportation/LandVehicle.java
+++ src/Transportation/LandVehicle.java
@@ -2,11 +2,23 @@ package Transportation;
 
 public abstract class LandVehicle extends Vehicle
 {
+	/**
+	 * Available road types.
+	 */
 	public static enum RoadType { Asphalt, Dirt };
 
 	private int wheels_count;
 	private RoadType road_type;
 	
+	/**
+	 * Constructor for class LandVehicle
+	 * 
+	 * @param model_name    vehicle's model name.
+	 * @param max_speed     vehicle's top speed.
+	 * @param max_seats     vehicle's seat count.
+	 * @param wheels_count  vehicle's wheel count.
+	 * @param road_type     vehicle's intended road type.
+	 */
 	public LandVehicle(String model_name, float max_speed, int max_seats, int wheels_count, RoadType road_type)
 	{
 		super(model_name, max_speed, max_seats);
@@ -14,16 +26,26 @@ public abstract class LandVehicle extends Vehicle
 		this.road_type = road_type;
 	}
 	
+	/**
+	 * Method for retrieving vehicle's wheels count.
+	 * @return integer value of the wheels count.
+	 */
 	public int GetWheelsCount()
 	{
 		return this.wheels_count;
 	}
 	
+	/**
+	 * Method for retrieving vehicle's intended road type.
+	 * 
+	 * @return string containing the intended road type.
+	 */
 	public String GetRoadType()
 	{
 		return this.road_type.name();
 	}
 	
+	@Override
 	public boolean equals(Object obj)
 	{
 		if (!super.equals(obj))
@@ -47,6 +69,7 @@ public abstract class LandVehicle extends Vehicle
 		return true;
 	}
 	
+	@Override
 	public String toString()
 	{
 		return super.toString() + ", Wheels count: " + this.GetWheelsCount() + ", Road type: " + this.GetRoadType();
