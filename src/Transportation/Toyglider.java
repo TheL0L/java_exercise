8ff57f0d694cafa7c56e304ec187d7f45267e65b
yaruboy011@gmail.com <yaruboy011@gmail.com>
2023-04-14 19:27:57 +0300

    add Toy glider class

---
+++ src/Transportation/Toyglider.java
@@ -0,0 +1,51 @@
+package Transportation;
+
+public class Toyglider extends AerialVehicle implements NotMotorized
+{
+	private char energyScore;
+	private String powerSource;
+	
+	public Toyglider(String model_name,float max_speed,int max_seats,Type_of_use type,char energyScore )
+	{
+		super("Toy",10,0,Type_of_use.civilian);
+		this.energyScore = 'A';
+		this.powerSource = "manual";
+	}
+	public char EnergyScore()
+	{
+		return this.energyScore;
+	}
+	public String PowerSource()
+	{
+		return this.powerSource;
+	}
+	public boolean equals(Object obj)
+	{
+		if(!super.equals(obj))
+		{
+			return false; 
+		}
+		if(!(obj instanceof Spyglider))
+		{
+			return false; 
+		}
+		
+		Toyglider other = (Toyglider)obj;
+		if(this.energyScore!= other.energyScore)
+		{
+			return false;
+		}
+		if(this.powerSource != other.powerSource)
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	public String toString()
+	{
+		return "Spyglider: " + super.toString() + ",Enrgy score : " + this.EnergyScore()
+			+ " Power source:  " + this.PowerSource() + "  type of use: " + this.GetType_of_use();
+	}
+	
+}
