19b2a67996c64167600488ab4ddb6845f33d676e
Sergei <sergeiyakima1@gmail.com>
2023-05-09 00:04:12 +0300

    added documentaion for guielement class

--- src/graphics/GuiElement.java
+++ src/graphics/GuiElement.java
@@ -1,25 +1,38 @@
 package graphics;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 
+/**
+ * Class for holding label and another component next to each other,
+ * while being aligned to the left and right accordingly.
+ */
 public class GuiElement extends JPanel
 {
 	private JComponent component;
 	
-	public GuiElement(String label_string, JComponent component)
+	/**
+	 * Constructor class for GuiElement.
+	 * 
+	 * @param label_text  string holding the text for the label, cannot be changed.
+	 * @param component   any java.swing component to be held right of the label.
+	 */
+	public GuiElement(String label_text, JComponent component)
 	{
 		super(new BorderLayout());
 		this.component = component;
 		
-		this.add(new JLabel(label_string), BorderLayout.WEST);
+		this.add(new JLabel(label_text), BorderLayout.WEST);
 		this.add(this.component, BorderLayout.EAST);
 	}
 	
+	/**
+	 * Method for retrieving the component's reference.
+	 * 
+	 * @return the component's reference.
+	 */
 	public JComponent GetComponent()
 	{
 		return this.component;
