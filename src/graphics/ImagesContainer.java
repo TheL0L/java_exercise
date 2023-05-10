72744a2408dd9f601d2629a769641a3081eba938
Sergei <sergeiyakima1@gmail.com>
2023-05-10 23:44:06 +0300

    added documentaion for ImagesContainer class

--- src/graphics/ImagesContainer.java
+++ src/graphics/ImagesContainer.java
@@ -8,6 +8,9 @@ import java.util.Vector;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
+/**
+ * Class for holding click-able images in a grid, with the ability for selecting either of them.
+ */
 public class ImagesContainer extends JPanel
 {
 	private Vector<ImageButton> images;
@@ -17,6 +20,15 @@ public class ImagesContainer extends JPanel
 	
 	private int selected_id;
 	
+	/**
+	 * Constructor class for ImagesContainer.
+	 * 
+	 * @param rows              integer value for the number of rows (set 0 for dynamic scaling).
+	 * @param columns           integer value for the number of columns (set 0 for dynamic scaling).
+	 * @param images_width      integer value for the width of all contained images.
+	 * @param images_height     integer value for the height of all contained images.
+	 * @param has_custom_image  boolean value dictating whether the user should be able to pick a custom image.
+	 */
 	public ImagesContainer(int rows, int columns, int images_width, int images_height, boolean has_custom_image)
 	{
         this.setLayout(new GridLayout(rows, columns, 15, 15));
@@ -35,18 +47,37 @@ public class ImagesContainer extends JPanel
         }
     }
 	
+	/**
+	 * Method for adding an image to the container.
+	 * 
+	 * @param file_path     string containing the file path to an image file.
+	 * @param tooltip_text  string containing the text to be used in the tooltip.
+	 */
 	public void AddImage(String file_path, String tooltip_text)
 	{
 		this.images.add(new ImageButton(file_path, tooltip_text, this.images_width, this.images_height));
 		this.add(this.images.lastElement());
 	}
 	
+	/**
+	 * Method for adding an image to the container.
+	 * 
+	 * @param image         image containing the image data to be used.
+	 * @param tooltip_text  string containing the text to be used in the tooltip.
+	 */
 	public void AddImage(Image image, String tooltip_text)
 	{
 		this.images.add(new ImageButton(image, tooltip_text, this.images_width, this.images_height));
 		this.add(this.images.lastElement());
 	}
 	
+	/**
+	 * Method for adding a click-able image to the container.
+	 * 
+	 * Used for adding pre-configured ImageButton elements with custom onClick methods.
+	 * 
+	 * @param image_button  the ImageButton element to be added.
+	 */
 	public void AddImageButton(ImageButton image_button)
 	{
 		this.images.add(image_button);
@@ -54,11 +85,19 @@ public class ImagesContainer extends JPanel
 		this.add(this.images.lastElement());
 	}
 	
+	/**
+	 * Method for retrieving the selected image in the container.
+	 * 
+	 * @return integer value of the image's index in the container.
+	 */
 	public int GetSelectedID()
 	{
 		return this.selected_id;
 	}
 	
+	/**
+	 * Method for clearing the image selection in the container.
+	 */
 	public void ClearSelection()
 	{
 		if (this.selected_id != -1)
@@ -68,6 +107,13 @@ public class ImagesContainer extends JPanel
 		this.selected_id = -1;
 	}
 	
+	/**
+	 * Method for setting the selected image in the container.
+	 * 
+	 * Draws a red border around the selected image.
+	 * 
+	 * @param image_reference the newly selected image's reference.
+	 */
 	public void SetSelected(ImageButton image_reference)
 	{
 		int id = this.images.indexOf(image_reference);
