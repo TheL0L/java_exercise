89d27149ae9a0989aac61561b5afdd06b479f0f5
Sergei <sergeiyakima1@gmail.com>
2023-05-10 19:17:53 +0300

    added a frame for toyglider creation, need to add functionality

---
+++ src/graphics/ToyGliderCreationFrame.java
@@ -0,0 +1,69 @@
+package graphics;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import Transportation.*;
+
+public class ToyGliderCreationFrame extends JFrame
+{
+	private final String path = "src/resources/vehicles/toy_glider/";
+	private final String[] files = {
+			"A.jpeg",
+			"B.jpeg",
+			"C.jpeg"
+	};
+	private final String[] tooltips = {
+		"",
+		"",
+		""
+	};
+	
+	private GuiElement field_source;
+	
+	public ToyGliderCreationFrame()
+	{
+		super("Toy Glider Creation");
+		this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.PAGE_AXIS));
+		
+
+		ImagesContainer images_container = new ImagesContainer(0, 4, 100, 100, true);
+		for (int i = 0; i < files.length; ++i)
+		{
+			images_container.AddImage(path + files[i], tooltips[i]);
+		}
+		this.add(images_container);
+		
+		
+		JButton btn_add = new JButton("Add Toy Glider");
+		btn_add.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO: check if inputs are valid, then add vehicle and/or close window
+				int option = JOptionPane.showConfirmDialog(
+						ToyGliderCreationFrame.this,
+					"Are you sure you want to close this window?",
+					"Confirm Close",
+					JOptionPane.YES_NO_OPTION
+				);
+				
+		        if (option == JOptionPane.YES_OPTION) {
+		            dispose(); // close the JFrame
+		        }
+			}
+		});
+		this.add(btn_add);
+		
+		this.pack();
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+	
+}
